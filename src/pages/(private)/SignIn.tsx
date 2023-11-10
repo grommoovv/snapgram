2afd16e21e01612a39173634403f965c9426b4e8
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-10 19:57:15 +0300

    init private route group

---
+++ src/pages/(private)/SignIn.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+// import cls from './SignIn.module.scss'
+
+interface SignInProps {}
+
+const SignIn: FC = (props) => {
+  return (
+    <>
+      <div>SignIn</div>
+    </>
+  )
+}
+
+export default SignIn
---
+++ src/pages/(private)/SignUp.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import { FC } from 'react'
+// import cls from './SignUp.module.scss'
+
+interface SignUpProps {}
+
+const SignUp: FC = (props) => {
+  return (
+    <>
+      <Button>Sign Up</Button>
+    </>
+  )
+}
+
+export default SignUp
