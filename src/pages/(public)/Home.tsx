8de5b5f5940d7a223ca9b740863b09734f9c7689
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-10 19:57:00 +0300

    init public route group

---
+++ src/pages/(public)/Home.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+// import cls from './Home.module.scss'
+
+interface HomeProps {}
+
+const Home: FC = (props) => {
+  return (
+    <>
+      <div>Home</div>
+    </>
+  )
+}
+
+export default Home
