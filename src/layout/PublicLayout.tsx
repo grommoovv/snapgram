7e280c855c9c13d730a9b4571ffdc1f56675bd84
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-10 19:57:36 +0300

    init layouts

---
+++ src/layout/PublicLayout.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+// import cls from './PublicLayout.module.scss'
+
+interface PublicLayoutProps {}
+
+const PublicLayout: FC = (props) => {
+  return (
+    <>
+      <div>PublicLayout</div>
+    </>
+  )
+}
+
+export { PublicLayout }
