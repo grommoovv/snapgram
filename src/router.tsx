bd041a7a229f8f3da6ffe76fb7f31c036d3ae08d
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-11 15:52:09 +0300

    create & setup router component

---
+++ src/router.tsx
@@ -0,0 +1,41 @@
+import { Routes, Route } from 'react-router-dom'
+import { PrivateLayout, PublicLayout } from './layout'
+import { SignIn, SignUp } from './pages/(private)'
+import {
+  Home,
+  Explore,
+  Saved,
+  People,
+  CreatePost,
+  EditPost,
+  PostDetails,
+  Profile,
+  UpdateProfile,
+} from './pages/(public)'
+
+const Router = () => {
+  return (
+    <Routes>
+      {/* public routes */}
+      <Route element={<PrivateLayout />}>
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Route>
+
+      {/* private routes */}
+      <Route element={<PublicLayout />}>
+        <Route index element={<Home />} />
+        <Route path='/explore' element={<Explore />} />
+        <Route path='/saved' element={<Saved />} />
+        <Route path='/people' element={<People />} />
+        <Route path='/create-post' element={<CreatePost />} />
+        <Route path='/update-post/:id' element={<EditPost />} />
+        <Route path='/posts/:id' element={<PostDetails />} />
+        <Route path='/profile/:id/*' element={<Profile />} />
+        <Route path='/update-profile/:id' element={<UpdateProfile />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export { Router }
