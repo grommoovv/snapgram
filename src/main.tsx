60219abf44c73570c10f2138338dd89e1a854fab
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-11 16:01:02 +0300

    setup app entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvider } from './context/auth.tsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { App } from './app.tsx'
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
