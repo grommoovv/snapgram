d885b2ac5ecaef3c0cfda90635a5ababbdbeb737
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-11 16:06:13 +0300

    export layouts

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,2 +1,6 @@
 export { PrivateLayout } from './PrivateLayout'
 export { PublicLayout } from './PublicLayout'
+
+export { Topbar } from './Topbar/Topbar'
+export { LeftSidebar } from './LeftSidebar/LeftSidebar'
+export { Bottombar } from './Bottombar/Bottombar'
