0df3a9dd412b6da8c019de7787a8b741067708af
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-27 18:37:36 +0300

    update user card styles

--- src/components/shared/UserCard.tsx
+++ src/components/shared/UserCard.tsx
@@ -16,12 +16,12 @@ const UserCard = ({ user }: UserCardProps) => {
         className='rounded-full w-14 h-14'
       />
 
-      <div className='flex-center flex-col gap-1'>
-        <p className='base-medium text-light-1 text-center line-clamp-1'>{user.name}</p>
-        <p className='small-regular text-light-3 text-center line-clamp-1'>@{user.username}</p>
+      <div className='flex flex-col gap-1'>
+        <p className='base-medium text-light-1  line-clamp-1'>{user.name}</p>
+        <p className='small-regular text-light-3  line-clamp-1'>@{user.username}</p>
       </div>
 
-      <Button type='button' size='sm' className='shad-button_primary px-5'>
+      <Button type='button' size='sm' className='shad-button_primary px-5 ml-[auto]'>
         Follow
       </Button>
     </Link>
