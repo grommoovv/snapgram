9af3d0311096b6204d40a0654942cf6eb349446b
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-27 18:38:11 +0300

    add rightsidebar component export

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -3,4 +3,5 @@ export { PublicLayout } from './PublicLayout'
 
 export { Topbar } from './Topbar/Topbar'
 export { LeftSidebar } from './LeftSidebar/LeftSidebar'
+export { RightSidebar } from './RightSidebar/RightSidebar'
 export { Bottombar } from './Bottombar/Bottombar'
